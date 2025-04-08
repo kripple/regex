cec9e8793fa550dd8cebbd62243317a64e2b1c42
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-04-08 16:34:28 -0500

    add fonts and colors

--- src/root.tsx
+++ src/root.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 
 import { App } from '@/components/App';
 
+import '@/colors.css';
 import '@/root.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(<App />);
